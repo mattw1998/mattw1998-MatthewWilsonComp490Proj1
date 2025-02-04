4ed4ac9d7fe178b1c35befb77a0634eab9bae5bb
mattw1998 <134012267+mattw1998@users.noreply.github.com>
2025-02-04 17:28:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 if __name__ == '__main__':
 
     # configure gemini AI w/ api key
-    genai.configure(api_key="AIzaSyCexoSxIJ372OV-j-h6Ln-fWg8mQUIjhoA")
+    genai.configure(api_key="ENTER API KEY HERE")
     model = genai.GenerativeModel("gemini-1.5-flash")
 
     job_title = input("Enter the title of a job posting: ")
@@ -11,23 +11,13 @@ if __name__ == '__main__':
     personal_description = input("Enter a description of yourself, including relevant skills: ")
 
     # send query to AI model and store response
-    query = ("Given the job, ", job_title, "with a description ", job_description, ". ",
-    "Give me a sample resume in markdown format, for someone with these skills. ",
-    personal_description)
-    response = model.generate_content(query)
-    print('Query 1 result: \n')
-    print(response.text)
-
-    query = ("Given the job, ", job_title, "with a description ", job_description, ". ",
-             "Give me a sample resume in markdown format. Relevant personal skills to be included are, ", personal_description)
-    response = model.generate_content(query)
-    print('\n\n\nQuery 2 result: \n')
-    print(response.text)
-
-    query = ("Based on these skills, ", personal_description,
+        query = ("Based on these skills, ", personal_description,
              ". Create a resume in markdown format for the job, ", job_title,
              ", with a description of ", job_description)
     response = model.generate_content(query)
     print('\n\n\nQuery 3 result: \n')
     print(response.text)
 
+    with open('resume.md', 'w') as file:
+        file.write(response.text)
+    
